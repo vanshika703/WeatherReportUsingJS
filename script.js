059b35282462a914999e9cb0e0bfc243fbf92d33
vanshika703 <vanshikabhojnagarwala@gmail.com>
2019-09-08 00:28:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+window.addEventListener('load',  () =>{
+    let long;
+    let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
+    let temperatureSection = document.querySelector('.temperature');
+    const temperatureSpan = document.querySelector('.temperature span');
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position =>{
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const proxy = "http://cors-anywhere.herokuapp.com/";
+            const api = `${proxy}https://api.darksky.net/forecast/73f34fe8800505eb6bf6f766c1631bce/${lat},${long}`;
+        
+            fetch(api)
+            .then(response => response.json())
+            .then(data => {
+                //to view everything that comes from the api
+
+                const {temperature, summary, icon} = data.currently;
+                //Set DOM Elements from the API
+                temperatureDegree.textContent = temperature;
+                temperatureDescription.textContent = summary;
+                locationTimezone.textContent = data.timezone;
+                
+                //formula
+                let celsius = (temperature - 32) * (5 / 9);
+                console.log(celsius);
+                
+                setIcons(icon,document.querySelector(".icon"));
+
+                //change from F to C
+                temperatureSection.addEventListener('click',() =>{
+                    if(temperatureSpan.textContent === "F"){
+                        temperatureSpan.textContent = "C";
+                        temperatureDegree.textContent = Math.floor(celsius);
+                    }else{
+                        temperatureSpan.textContent = "F";
+                        temperatureDegree.textContent = temperature;
+                    }
+                })
+
+            })
+        });
+    }
+
+    function setIcons(icon, iconID){
+        const skycons = new Skycons({color:"white"});
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
+    }
+    
+});
